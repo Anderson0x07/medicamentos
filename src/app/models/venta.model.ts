f99a58860f7a2d81ef57f9440942e3d059097d58
Anderson0x07 <andersonadrianorro@ufps.edu.co>
2023-09-11 15:33:45 -0500

    Fixed

--- src/app/models/venta.model.ts
+++
@@ -1,9 +0,0 @@
-export interface Venta {
-    id: string,
-    fecha_venta: string,
-    medicamento: string,
-    cantidad: number,
-    valor_unitario: number,
-    valor_total: number,
-    medicamentoDto: object
-}
